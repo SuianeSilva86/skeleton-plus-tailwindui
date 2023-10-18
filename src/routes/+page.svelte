607b91602e60c90dbb5defe2b776794ebe26a258
Suiane <maria.suine934@gmail.com>
2023-10-18 10:05:22 -0300

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,64 @@
+
+<section role="presentation" class="">
+	<!-- message -->
+	<div class="animate-pulse w-full px-5 flex flex-col justify-between ">
+		<div class="flex flex-col mt-5">
+			<div class="flex justify-end mb-4">
+				<div
+					class="mr-2 py-7 px-44 bg-gray-400 rounded-bl-3xl rounded-tl-3xl rounded-br-xl max-sm:px-9 max-sm:py-1 max-md:py-4"
+				/>
+				<div class=" object-cover w-7 h-7 rounded-full bg-gray-400 max-md:w-5 max-md:h-5" />
+			</div>
+			<div class="flex justify-start mb-4">
+				
+				<div class="object-cover w-7 h-7 rounded-full bg-gray-300 max-md:w-5 max-md:h-5" />
+				<div
+					class="ml-2 py-10 px-60 bg-gray-300 rounded-bl-xl rounded-tr-xl rounded-e-xl max-sm:px-24 max-sm:py-1 max-md:py-4"
+				/>
+			</div>
+			<div class="flex justify-end mb-4">
+				<div
+					class="mr-2 py-5 px-44 bg-gray-400 rounded-bl-3xl rounded-tl-3xl rounded-br-xl max-sm:px-20 max-sm:py-1 max-md:py-4"
+				/>
+				<div class=" object-cover w-7 h-7 rounded-full bg-gray-400 max-md:w-5 max-md:h-5" />
+			</div>
+			<div class="flex justify-end mb-4">
+				<div
+					class="mr-10 py-5 px-44 bg-gray-400  rounded-2xl max-sm:px-10 max-sm:py-1 max-md:py-2 max-sm:mr-6 max-md:mr-6"
+				/>
+			
+			</div>
+			<div class="flex justify-start mb-4">
+				<div class=" object-cover w-7 h-7 rounded-full bg-gray-300 max-md:w-5 max-md:h-5" />
+				<div
+					class="ml-2 py-8 px-60 bg-gray-300 rounded-bl-2xl rounded-tr-xl rounded-e-xl max-sm:px-24 max-sm:py-1 max-md:py-4"
+				/>
+			</div>
+			<div class="flex justify-start mb-4">
+				<div
+					class="ml-10 py-4 px-52 bg-gray-300 rounded-2xl 
+					max-sm:px-10 
+					max-sm:py-1
+					max-sm:ml-6 
+					max-md:py-1
+					max-md:ml-6"
+				/>
+			</div>
+			
+		</div>
+	</div>
+</section>
+<br><br><br><br><br><br><br><br>
+
+<!-- Loading Chat "digitando" -->
+<section>
+	<div class="flex items-center justify-center">
+
+		<div class="flex space-x-2 animate-pulse">
+			<div class="w-3 h-3 bg-gray-500 rounded-full"></div>
+			<div class="w-3 h-3 bg-gray-500 rounded-full"></div>
+			<div class="w-3 h-3 bg-gray-500 rounded-full"></div>
+		</div>
+	
+	</div>
+</section>
